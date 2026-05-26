07d220f836a7c24f19ef444ce858252a9edef586
Deepak Sahu <sdeepaksahu1@gmail.com>
2026-05-26 16:08:38 +0530

    remove credentials for production

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,7 +9,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: process.env.CLIENT_URL, // Adjust this to your frontend URL
-    credentials: true, // Allow cookies to be sent with requests
+    // credentials: true, // Allow cookies to be sent with requests
   })
 );
 
